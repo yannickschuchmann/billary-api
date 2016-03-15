2b8c2a60d65451af3d6e4a973db4b76ef64fe86c
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2016-03-15 14:25:40 +0100

    remove forgotten "debugger"

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -58,7 +58,6 @@ class TimeEntriesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_time_entry
-      debugger
       @time_entry = TimeEntry.find(params[:id])
     end
 
