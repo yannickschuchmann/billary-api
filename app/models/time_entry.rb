945f2875afa0ffe36905b66e24209f773b786dc2
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2016-03-16 14:29:31 +0100

    cut seconds from stopped TimeEntry

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -2,6 +2,7 @@ class TimeEntry < ApplicationRecord
   belongs_to :project
 
   before_validation :set_started_at
+  before_validation :round_dates, if: "stopped_at.present?"
   before_create :stop_all_others, unless: "stopped_at.present?"
 
   validates :started_at, presence: true
@@ -9,6 +10,7 @@ class TimeEntry < ApplicationRecord
 
   def duration
     if self.stopped_at.present?
+      self.round_dates # set this even when self is not saved yet.
       return ((self.stopped_at - self.started_at) / 60).ceil
     else
       return 0
@@ -23,17 +25,26 @@ class TimeEntry < ApplicationRecord
     TimeEntry.all.where(stopped_at: nil).update_all(stopped_at: Time.now)
   end
 
+  def round_dates
+    duration = ((self.stopped_at - self.started_at) / 60).ceil
+
+    self.started_at = self.started_at.beginning_of_minute
+    self.stopped_at = self.started_at + duration.minutes
+  end
+
   private
   def set_started_at
     self.started_at = Time.now if self.started_at.blank?
   end
+
 
   def stop_all_others
     TimeEntry.stop_all
   end
 
   def stopped_greater_than_started
-    errors.add('Stopped at date', 'must be greater than started at') unless self.started_at.present? &&
-        self.stopped_at > self.started_at
+    unless self.started_at.present? && self.stopped_at > self.started_at
+      errors.add('Stopped at date', 'must be greater than started at')
+    end
   end
 end
