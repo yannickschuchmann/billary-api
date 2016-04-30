66a710ca970337eaa28c1c190ff246ab807c89a5
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2016-04-30 16:35:53 +0200

    Add open duration to projects endpoint

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,5 +1,6 @@
 class ProjectSerializer < ActiveModel::Serializer
   attributes :id, :name, :duration, :child_ids, :parent_id, :client_id
+  attribute :duration_of_open_time_entries, key: :open_duration
 
   has_many :time_entries, serializer: TimeEntrySerializer
   belongs_to :client, serializer: ClientSerializer
